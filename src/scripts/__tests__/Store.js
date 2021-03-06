5b94d2991d872d74334e16abddea1bd4c7c8866a
St3ve89 <istvan.acs89@gmail.com>
2021-03-06 20:31:44 +0000

    Add tests cases for filtering by productType

--- src/scripts/__tests__/Store.js
+++ src/scripts/__tests__/Store.js
@@ -2,15 +2,68 @@ import Store from "../Store";
 import mockData from "../../../public/db.json";
 
 describe("filter", () => {
-  it("should return all deals when no filters applied", () => {
-    // Arrange
-    const sut = new Store();
-    sut.setDeals(mockData.deals);
+  let sut;
 
+  beforeEach(() => {
+    // Arrange
+    sut = new Store();
+    sut.setDeals(mockData.deals);
+  });
+
+  it("should return all deals when no filters applied", () => {
     // Act
     const result = sut.deals;
 
     // Assert
     expect(result).toEqual(mockData.deals);
+    expect(result).toHaveLength(11);
+  });
+
+  it("should return 4 deals if filter by broadband", () => {
+    // Arrange
+    sut.setProductFilter("broadband");
+
+    // Act
+    const result = sut.deals;
+
+    // Assert
+    expect(result).toHaveLength(4);
+  });
+
+  it("should return 4 deals if filter by broadband and tv", () => {
+    // Arrange
+    sut.setProductFilter("broadband");
+    sut.setProductFilter("tv");
+
+    // Act
+    const result = sut.deals;
+
+    // Assert
+    expect(result).toHaveLength(4);
+  });
+
+  it("should return 1 deals if filter by broadband and mobile", () => {
+    // Arrange
+    sut.setProductFilter("broadband");
+    sut.setProductFilter("mobile");
+
+    // Act
+    const result = sut.deals;
+
+    // Assert
+    expect(result).toHaveLength(1);
+  });
+
+  it("should return 2 deals if filter by broadband, tv and mobile", () => {
+    // Arrange
+    sut.setProductFilter("broadband");
+    sut.setProductFilter("tv");
+    sut.setProductFilter("mobile");
+
+    // Act
+    const result = sut.deals;
+
+    // Assert
+    expect(result).toHaveLength(2);
   });
 });
